8215ac8ce96e0c17842b525b8e8af0cf579e1fa6
Elalfy74 <mahmoudelalfy13@gmail.com>
2024-05-05 23:12:29 +0300

    initial commit (setup App)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: Infinity,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </HydrationBoundary>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
